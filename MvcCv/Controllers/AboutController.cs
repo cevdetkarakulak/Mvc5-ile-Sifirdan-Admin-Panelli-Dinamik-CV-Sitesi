db34a15c1b11495edb56af8603ac72a0e6057688
c3vdet <cevdetkarakulak@hotmail.com>
2024-03-27 22:40:17 +0300

    Add project files.

---
+++ MvcCv/Controllers/AboutController.cs
@@ -0,0 +1,34 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class AboutController : Controller
+    {
+        GenericRepository<TblAbout> repo = new GenericRepository<TblAbout>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult Index(TblAbout tblAbout)
+        {
+            var values = repo.Fınd(x => x.AboutID == 1);
+            values.Name = tblAbout.Name;
+            values.SurName = tblAbout.SurName;
+            values.Mail = tblAbout.Mail;
+            values.Phone = tblAbout.Phone;
+            values.ImageUrl = tblAbout.ImageUrl;
+            values.Adress = tblAbout.Adress;
+            repo.TUpdate(values);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCv/Controllers/AwardController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class AwardController : Controller
+    {
+        // GET: Award
+        GenericRepository<TblAward> repo = new GenericRepository<TblAward>();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AwardUpdate(int id)
+        {
+            ViewBag.d = id;
+            var values = repo.Fınd(x => x.AwardID == id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult AwardUpdate(TblAward tblAward)
+        {
+            var values = repo.Fınd(x => x.AwardID == tblAward.AwardID);
+            values.explanation = tblAward.explanation;
+            values.Date = tblAward.Date;
+            repo.TUpdate(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AwardAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AwardAdd(TblAward tblAward)
+        {
+            repo.TAdd(tblAward);
+            return RedirectToAction("Index");
+        }
+        public ActionResult AwardDelete(int id)
+        {
+            var values = repo.Fınd(x => x.AwardID == id);
+            repo.TDelete(values);
+            return RedirectToAction("Index");
+        }
+    }
+}
